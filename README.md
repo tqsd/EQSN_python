0e175d054eebae06929589f1bb892baca0f0c716
Benjamin <benjamin.zanger@gmx.de>
2020-03-05 17:08:36 +0100

    making reposiory ready for pypi

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 EQSN is a library for simulating quantum networks.
 It is developed as a backend for the
 [QuNetSim](https://github.com/tqsd/QuNetSim) package, but can also be used as a standalone library.
+
+The documentation can be found [here](https://tqsd.github.io/EQSN_python/_build/index.html).
--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='EQSN',
     version='0.0.1',
     packages=['eqsn',],
-    license='',
+    license='MIT',
+    author='Benjamin Zanger',
+    author_email='benjamin.zanger@tum.de',
+    description="A quantum simulator made for networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/tqsd/EQSN_python',
+    download_url = '',
+    keywords = ['quantum-simulator', 'quantum-internet', 'QuNetSim'],
+    install_requires=[
+        'numpy',
+        'multiprocessing',
+      ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3.6',
 )

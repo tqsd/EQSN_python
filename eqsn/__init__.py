b2c21ea94bd52fb82d8039634ccbbdf7866bd729
Benjamin <benjamin.zanger@gmx.de>
2020-03-06 12:37:21 +0100

    add new gates to eqsn package

--- eqsn/__init__.py
+++ eqsn/__init__.py
@@ -1,2 +1,3 @@
 from eqsn.gates import X_gate, Y_gate, Z_gate, cnot_gate, cphase_gate, H_gate, \
-                    RX_gate, RY_gate, RZ_gate, new_qubit, stop_all, measure
+                    RX_gate, RY_gate, RZ_gate, new_qubit, stop_all, measure,\
+                    S_gate, T_gate, K_gate

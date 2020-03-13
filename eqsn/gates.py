d88df33959aa742c32e5d1aa04a1add9998deedb
Benjamin <benjamin.zanger@gmx.de>
2020-03-13 12:11:33 +0100

    changed sahred dictionary to a singleton object

--- eqsn/gates.py
+++ eqsn/gates.py
@@ -3,9 +3,7 @@ import numpy as np
 from eqsn.qubit_thread import SINGLE_GATE, MERGE_SEND, MERGE_ACCEPT, MEASURE,\
                 MEASURE_NON_DESTRUCTIVE, GIVE_QUBITS_AND_TERMINATE, \
                 CONTROLLED_GATE, QubitThread
-from eqsn.shared_dict import get_queues_for_ids, set_thread_with_id, \
-    send_all_threads, stop_all_threads, \
-    change_thread_and_queue_of_ids_and_join, delete_id_and_check_to_join_thread
+from eqsn.shared_dict import SharedDict
 
 
 class EQSN(object):
@@ -17,6 +15,7 @@ class EQSN(object):
 
     def __init__(self):
         self.manager = multiprocessing.Manager()
+        self.shared_dict = SharedDict.get_instance()
 
     def new_qubit(self, id):
         """
@@ -25,22 +24,22 @@ class EQSN(object):
         q = multiprocessing.Queue()
         thread = QubitThread(id, q)
         p = multiprocessing.Process(target=thread.run, args=())
-        set_thread_with_id(id, p, q)
+        self.shared_dict.set_thread_with_id(id, p, q)
         p.start()
 
     def stop_all(self):
         """
         Stops the simulator from running.
         """
-        send_all_threads(None)
-        stop_all_threads()
+        self.shared_dict.send_all_threads(None)
+        self.shared_dict.stop_all_threads()
 
     def X_gate(self, q_id):
         """
         Applies the Pauli X gate to the Qubit with q_id.
         """
         x = np.array([[0, 1], [1, 0]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def Y_gate(self, q_id):
@@ -48,7 +47,7 @@ class EQSN(object):
         Applies the Pauli Y gate to the Qubit with q_id.
         """
         x = np.array([[0, 0 - 1j], [0 + 1j, 0]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def Z_gate(self, q_id):
@@ -56,7 +55,7 @@ class EQSN(object):
         Applies the Pauli Z gate to the Qubit with q_id.
         """
         x = np.array([[1, 0], [0, -1]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def H_gate(self, q_id):
@@ -64,7 +63,7 @@ class EQSN(object):
         Applies the Hadamard gate to the Qubit with q_id.
         """
         x = (1 / 2.0) ** 0.5 * np.array([[1, 1], [1, -1]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def T_gate(self, q_id):
@@ -72,7 +71,7 @@ class EQSN(object):
         Applies the T gate to the Qubit with q_id.
         """
         x = np.array([[1, 0], [0, (0.7071067811865476 + 0.7071067811865475j)]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def S_gate(self, q_id):
@@ -80,7 +79,7 @@ class EQSN(object):
         Applies the S gate to the Qubit with q_id.
         """
         x = np.array([[1, 0], [0, 1j]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def K_gate(self, q_id):
@@ -88,7 +87,7 @@ class EQSN(object):
         Applies the K gate to the Qubit with q_id.
         """
         x = 0.5 * np.array([[1+1j, 1-1j], [-1+1j, -1-1j]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def RX_gate(self, q_id, rad):
@@ -98,7 +97,7 @@ class EQSN(object):
         mid = np.cos(rad / 2)
         other = -1j * np.sin(rad / 2)
         x = np.array([[mid, other], [other, mid]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def RY_gate(self, q_id, rad):
@@ -108,7 +107,7 @@ class EQSN(object):
         mid = np.cos(rad / 2)
         other = np.sin(rad / 2)
         x = np.array([[mid, -1.0 * other], [other, mid]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def RZ_gate(self, q_id, rad):
@@ -118,7 +117,7 @@ class EQSN(object):
         top = np.exp(-1j * (rad / 2))
         bot = np.exp(1j * (rad / 2))
         x = np.array([[top, 0], [0, bot]], dtype=np.csingle)
-        q = get_queues_for_ids([q_id])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id])[0]
         q.put([SINGLE_GATE, x, q_id])
 
     def merge_qubits(self, q_id1, q_id2):
@@ -126,7 +125,7 @@ class EQSN(object):
         Merges two qubits to one process, if they are not already
         running in the same process.
         """
-        l = get_queues_for_ids([q_id1, q_id2])
+        l = self.shared_dict.get_queues_for_ids([q_id1, q_id2])
         if len(l) == 1:
             return  # Already merged
         else:
@@ -139,7 +138,7 @@ class EQSN(object):
             qubits_q = self.manager.Queue()
             q1.put([GIVE_QUBITS_AND_TERMINATE, qubits_q])
             qubits = qubits_q.get()
-            change_thread_and_queue_of_ids_and_join(qubits, q_id2)
+            self.shared_dict.change_thread_and_queue_of_ids_and_join(qubits, q_id2)
 
     def cnot_gate(self, q_id1, q_id2):
         """
@@ -148,7 +147,7 @@ class EQSN(object):
         """
         x = np.array([[0, 1], [1, 0]], dtype=np.csingle)
         self.merge_qubits(q_id1, q_id2)
-        q = get_queues_for_ids([q_id1])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id1])[0]
         q.put([CONTROLLED_GATE, x, q_id1, q_id2])
 
     def cphase_gate(self, q_id1, q_id2):
@@ -158,7 +157,7 @@ class EQSN(object):
         """
         x = np.array([[0, 1], [0, -1]], dtype=np.csingle)
         self.merge_qubits(q_id1, q_id2)
-        q = get_queues_for_ids([q_id1])[0]
+        q = self.shared_dict.get_queues_for_ids([q_id1])[0]
         q.put([CONTROLLED_GATE, x, q_id1, q_id2])
 
     def measure(self, id, non_destructive=False):
@@ -168,12 +167,12 @@ class EQSN(object):
         measurement, but its wavefunction collapses.
         """
         ret = self.manager.Queue()
-        q = get_queues_for_ids([id])[0]
+        q = self.shared_dict.get_queues_for_ids([id])[0]
         if non_destructive:
             q.put([MEASURE_NON_DESTRUCTIVE, id, ret])
         else:
             q.put([MEASURE, id, ret])
         res = ret.get()
         if not non_destructive:
-            delete_id_and_check_to_join_thread(id)
+            self.shared_dict.delete_id_and_check_to_join_thread(id)
         return res
--- eqsn/shared_dict.py
+++ eqsn/shared_dict.py
@@ -1,158 +1,163 @@
 import threading
 
 
-# From O'Reilly Python Cookbook by David Ascher, Alex Martelli
-# With changes to cover the starvation situation where a continuous
-#   stream of readers may starve a writer, Lock Promotion and Context Managers
-class ReadWriteLock:
-    """
-    A lock object that allows many simultaneous "read locks", but
-    only one "write lock."
-    """
+class SharedDict(object):
 
-    def __init__(self, with_promotion=False):
-        self._read_ready = threading.Condition(threading.RLock())
-        self._readers = 0
-        self._writers = 0
-        self._promote = with_promotion
-        self._readerList = []  # List of Reader thread IDs
-        self._writerList = []  # List of Writer thread IDs
+    # From O'Reilly Python Cookbook by David Ascher, Alex Martelli
+    # With changes to cover the starvation situation where a continuous
+    #   stream of readers may starve a writer, Lock Promotion and Context Managers
+    class ReadWriteLock:
+        """
+        A lock object that allows many simultaneous "read locks", but
+        only one "write lock."
+        """
 
-    def acquire_read(self):
-        """
-        Acquire a read lock. Blocks only if a thread has
-        acquired the write lock.
-        """
-        self._read_ready.acquire()
-        try:
-            while self._writers > 0:
-                self._read_ready.wait()
-                self._readers += 1
-        finally:
-            self._readerList.append(threading.get_ident())
+        def __init__(self, with_promotion=False):
+            self._read_ready = threading.Condition(threading.RLock())
+            self._readers = 0
+            self._writers = 0
+            self._promote = with_promotion
+            self._readerList = []  # List of Reader thread IDs
+            self._writerList = []  # List of Writer thread IDs
+
+        def acquire_read(self):
+            """
+            Acquire a read lock. Blocks only if a thread has
+            acquired the write lock.
+            """
+            self._read_ready.acquire()
+            try:
+                while self._writers > 0:
+                    self._read_ready.wait()
+                    self._readers += 1
+            finally:
+                self._readerList.append(threading.get_ident())
+                self._read_ready.release()
+
+        def release_read(self):
+            """
+            Release a read lock.
+            """
+            self._read_ready.acquire()
+            try:
+                self._readers -= 1
+                if not self._readers:
+                    self._read_ready.notifyAll()
+            finally:
+                self._readerList.remove(threading.get_ident())
+                self._read_ready.release()
+
+        def acquire_write(self):
+            """
+            Acquire a write lock. Blocks until there are no
+            acquired read or write locks.
+            """
+            self._read_ready.acquire()
+            self._writers += 1
+            self._writerList.append(threading.get_ident())
+            while self._readers > 0:
+                if self._promote and threading.get_ident() in self._readerList and set(self._readerList).issubset(
+                set(self._writerList)):
+                    break
+                else:
+                    self._read_ready.wait()
+
+        def release_write(self):
+            """
+            Release a write lock.
+            """
+            self._writers -= 1
+            self._writerList.remove(threading.get_ident())
+            self._read_ready.notifyAll()
             self._read_ready.release()
 
-    def release_read(self):
-        """
-        Release a read lock.
-        """
-        self._read_ready.acquire()
-        try:
-            self._readers -= 1
-            if not self._readers:
-                self._read_ready.notifyAll()
-        finally:
-            self._readerList.remove(threading.get_ident())
-            self._read_ready.release()
+    __instance = None
 
-    def acquire_write(self):
-        """
-        Acquire a write lock. Blocks until there are no
-        acquired read or write locks.
-        """
-        self._read_ready.acquire()
-        self._writers += 1
-        self._writerList.append(threading.get_ident())
-        while self._readers > 0:
-            if self._promote and threading.get_ident() in self._readerList and set(self._readerList).issubset(
-                    set(self._writerList)):
-                break
-            else:
-                self._read_ready.wait()
+    @staticmethod
+    def get_instance():
+        if SharedDict.__instance is None:
+            return SharedDict()
+        return SharedDict.__instance
 
-    def release_write(self):
-        """
-        Release a write lock.
-        """
-        self._writers -= 1
-        self._writerList.remove(threading.get_ident())
-        self._read_ready.notifyAll()
-        self._read_ready.release()
+    def __init__(self):
+        if SharedDict.__instance is not None:
+            raise Exception("Call get instance to get this class!")
+        SharedDict.__instance = self
+        self.lock = SharedDict.ReadWriteLock()
+        self.id_to_queue = {}
 
+        self.id_to_thread = {}
+        self.thread_list = []
+        self.queue_list = []
 
-id_to_queue = {}
-lock = ReadWriteLock()
+    def get_queues_for_ids(self, q_id_list):
+        ret = []
+        self.lock.acquire_read()
+        for id in q_id_list:
+            res = self.id_to_queue[id]
+            if res not in ret:
+                ret.append(res)
+        self.lock.release_read()
+        return ret
 
-id_to_thread = {}
-thread_list = []
-queue_list = []
+    def set_thread_with_id(self, q_id, thread, queue):
+        self.lock.acquire_write()
+        self.id_to_queue[q_id] = queue
+        self.id_to_thread[q_id] = thread
+        self.queue_list.append(queue)
+        self.thread_list.append(thread)
+        self.lock.release_write()
 
+    def delete_id(self, q_id):
+        self.lock.acquire_write()
+        del self.id_to_queue[q_id]
+        del self.id_to_thread[q_id]
+        self.lock.release_write()
 
-def get_queues_for_ids(q_id_list):
-    ret = []
-    lock.acquire_read()
-    for id in q_id_list:
-        res = id_to_queue[id]
-        if res not in ret:
-            ret.append(res)
-    lock.release_read()
-    return ret
+    def delete_id_and_check_to_join_thread(self, q_id):
+        self.lock.acquire_write()
+        thread = None
+        if q_id in self.id_to_thread.keys():
+            thread = self.id_to_thread[q_id]
+            if not thread.is_alive():
+                thread.join()
+                if thread in self.thread_list:
+                    self.thread_list.remove(thread)
+            del self.id_to_thread[q_id]
+            del self.id_to_queue[q_id]
+        self.lock.release_write()
 
+    def join_thread_with_id(self, q_id):
+        self.lock.acquire_read()
+        thread = self.id_to_thread[q_id]
+        self.lock.release_read()
+        thread.join()
+        self.lock.acquire_write()
+        self.thread_list.remove(thread)
+        del self.id_to_thread[q_id]
+        self.lock.release_write()
 
-def set_thread_with_id(q_id, thread, queue):
-    lock.acquire_write()
-    id_to_queue[q_id] = queue
-    id_to_thread[q_id] = thread
-    queue_list.append(queue)
-    thread_list.append(thread)
-    lock.release_write()
+    def change_thread_and_queue_of_ids_and_join(self, q_ids, q_id_new_thread):
+        self.lock.acquire_write()
+        new_thread = self.id_to_thread[q_id_new_thread]
+        new_queue = self.id_to_queue[q_id_new_thread]
+        for q_id in q_ids:
+            thread = self.id_to_thread[q_id]
+            if thread in self.thread_list:
+                self.thread_list.remove(thread)
+                thread.join()
+            self.id_to_thread[q_id] = new_thread
+            self.id_to_queue[q_id] = new_queue
+        self.lock.release_write()
 
+    def send_all_threads(self, msg):
+        self.lock.acquire_write()
+        for p in self.queue_list:
+            p.put(msg)
+        self.lock.release_write()
 
-def delete_id(q_id):
-    lock.acquire_write()
-    del id_to_queue[q_id]
-    del id_to_thread[q_id]
-    lock.release_write()
-
-
-def delete_id_and_check_to_join_thread(q_id):
-    lock.acquire_write()
-    thread = None
-    if q_id in id_to_thread.keys():
-        thread = id_to_thread[q_id]
-        if not thread.is_alive():
-            thread.join()
-            if thread in thread_list:
-                thread_list.remove(thread)
-        del id_to_thread[q_id]
-        del id_to_queue[q_id]
-    lock.release_write()
-
-
-def join_thread_with_id(q_id):
-    lock.acquire_read()
-    thread = id_to_thread[q_id]
-    lock.release_read()
-    thread.join()
-    lock.acquire_write()
-    thread_list.remove(thread)
-    del id_to_thread[q_id]
-    lock.release_write()
-
-
-def change_thread_and_queue_of_ids_and_join(q_ids, q_id_new_thread):
-    lock.acquire_write()
-    new_thread = id_to_thread[q_id_new_thread]
-    new_queue = id_to_queue[q_id_new_thread]
-    for q_id in q_ids:
-        thread = id_to_thread[q_id]
-        if thread in thread_list:
-            thread_list.remove(thread)
-            thread.join()
-        id_to_thread[q_id] = new_thread
-        id_to_queue[q_id] = new_queue
-    lock.release_write()
-
-
-def send_all_threads(msg):
-    lock.acquire_write()
-    for p in queue_list:
-        p.put(msg)
-    lock.release_write()
-
-
-def stop_all_threads():
-    lock.acquire_write()
-    for p in thread_list:
-        p.join()
-    lock.release_write()
+    def stop_all_threads(self):
+        self.lock.acquire_write()
+        for p in self.thread_list:
+            p.join()
+        self.lock.release_write()

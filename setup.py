18e0d76441940cac4c4a9441dd34f27df1d49433
Benjamin Zanger <benjamin.zanger@gmx.de>
2020-03-16 10:49:00 +0100

    updated for new release

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='EQSN',
-    version='0.0.2',
-    packages=['eqsn',],
+    version='0.0.3',
+    packages=['eqsn', ],
     license='MIT',
     author='Benjamin Zanger',
     author_email='benjamin.zanger@tum.de',
     description="A quantum simulator made for networks",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url = 'https://github.com/tqsd/EQSN_python',
-    download_url = 'https://github.com/tqsd/EQSN_python/releases/tag/v0.0.2',
-    keywords = ['quantum-simulator', 'quantum-internet', 'QuNetSim'],
+    url='https://github.com/tqsd/EQSN_python',
+    download_url='https://github.com/tqsd/EQSN_python/releases/tag/v0.0.3',
+    keywords=['quantum-simulator', 'quantum-internet', 'QuNetSim'],
     install_requires=[
         'numpy',
       ],

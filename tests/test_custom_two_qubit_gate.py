63c29680b3b35e33e974863993e1a9ddb8ed2f46
Stephen DiAdamo <stephen.diadamo@riverlane.com>
2020-07-09 11:30:36 +0100

    Adding three qubit custom controlled gate - Adds some tests - Fixes some typos - Renames shadowed variables in some parts of the code - Adds some doc strings

--- tests/test_custom_two_qubit_gate.py
+++ tests/test_custom_two_qubit_gate.py
@@ -1,5 +1,6 @@
-from eqsn import EQSN
 import time
+
+from eqsn import EQSN
 import numpy as np
 
 
@@ -19,5 +20,84 @@ def test_two_qubit_gate():
     eqsn.stop_all()
 
 
+def test_custom_two_qubit_control_gate_control_applied():
+    eqsn = EQSN()
+    eqsn.new_qubit("1")
+    eqsn.new_qubit("2")
+    eqsn.new_qubit("3")
+
+    eqsn.X_gate("1")
+    eqsn.X_gate("2")
+    # Swap gate
+    custom_gate = np.asarray(
+        [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]
+    )
+    eqsn.custom_two_qubit_control_gate("1", "2", "3", custom_gate)
+
+    m1 = eqsn.measure("1")
+    m2 = eqsn.measure("2")
+    m3 = eqsn.measure("3")
+
+    assert m2 == 0
+    assert m3 == 1
+    assert m1 == 1
+
+    eqsn.stop_all()
+
+
+def test_custom_two_qubit_control_gate_control_not_applied():
+    eqsn = EQSN()
+    eqsn.new_qubit("1")
+    eqsn.new_qubit("2")
+    eqsn.new_qubit("3")
+    eqsn.X_gate("2")
+    # Swap gate
+    custom_gate = np.asarray(
+        [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]
+    )
+    eqsn.custom_two_qubit_control_gate("1", "2", "3", custom_gate)
+
+    m1 = eqsn.measure("1")
+    m2 = eqsn.measure("2")
+    m3 = eqsn.measure("3")
+
+    assert m2 == 1
+    assert m3 == 0
+    assert m1 == 0
+
+    eqsn.stop_all()
+
+
+def test_custom_two_qubit_control_gate_control_ccnot():
+    eqsn = EQSN()
+    eqsn.new_qubit("1")
+    eqsn.new_qubit("2")
+    eqsn.new_qubit("3")
+
+    eqsn.X_gate("1")
+    eqsn.X_gate("2")
+    # Swap gate
+    custom_gate = np.asarray(
+        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
+    )
+    eqsn.custom_two_qubit_control_gate("1", "2", "3", custom_gate)
+
+    m1 = eqsn.measure("1")
+    m2 = eqsn.measure("2")
+    m3 = eqsn.measure("3")
+
+    assert m2 == 1
+    assert m3 == 1
+    assert m1 == 1
+
+    eqsn.stop_all()
+
+
 if __name__ == "__main__":
-    test_two_qubit_gate()
+    test_list = [test_two_qubit_gate,
+                 test_custom_two_qubit_control_gate_control_applied,
+                 test_custom_two_qubit_control_gate_control_not_applied,
+                 test_custom_two_qubit_control_gate_control_ccnot]
+    for func in test_list:
+        func()
+        time.sleep(0.5)

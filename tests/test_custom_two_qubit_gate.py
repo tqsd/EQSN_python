51fde4089f590a17d897d59a02b7456345c2943d
Stephen DiAdamo <stephen.diadamo@riverlane.com>
2020-07-09 11:33:44 +0100

    Adding one extra test to verify other qubits don't get affected by the gate swapping

--- tests/test_custom_two_qubit_gate.py
+++ tests/test_custom_two_qubit_gate.py
@@ -93,11 +93,43 @@ def test_custom_two_qubit_control_gate_control_ccnot():
     eqsn.stop_all()
 
 
+def test_custom_two_qubit_control_gate_other_qubits_not_affected():
+    eqsn = EQSN()
+    eqsn.new_qubit("4")
+    eqsn.new_qubit("2")
+    eqsn.new_qubit("3")
+    eqsn.new_qubit("1")
+    eqsn.new_qubit("5")
+
+    eqsn.X_gate("1")
+    eqsn.X_gate("2")
+    # Swap gate
+    custom_gate = np.asarray(
+        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
+    )
+    eqsn.custom_two_qubit_control_gate("1", "2", "3", custom_gate)
+
+    m1 = eqsn.measure("1")
+    m2 = eqsn.measure("2")
+    m3 = eqsn.measure("3")
+    m4 = eqsn.measure("4")
+    m5 = eqsn.measure("5")
+
+    assert m2 == 1
+    assert m3 == 1
+    assert m1 == 1
+    assert m4 == 0
+    assert m5 == 0
+
+    eqsn.stop_all()
+
+
 if __name__ == "__main__":
     test_list = [test_two_qubit_gate,
                  test_custom_two_qubit_control_gate_control_applied,
                  test_custom_two_qubit_control_gate_control_not_applied,
-                 test_custom_two_qubit_control_gate_control_ccnot]
+                 test_custom_two_qubit_control_gate_control_ccnot,
+                 test_custom_two_qubit_control_gate_other_qubits_not_affected]
     for func in test_list:
         func()
         time.sleep(0.5)
